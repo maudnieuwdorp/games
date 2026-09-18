32217d8f1307ad93a668d90597679aab57a9eab8
maud <maudnieuwdorp4@gmail.com>
2026-09-18 10:50:06 +0200

    Breeze authenticatie geïnstalleerd'

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,20 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('games', [GameController::class, 'index']);
+
+
+Route::get('games/create', [GameController::class, 'create']);
+Route::post('games/store', [GameController::class, 'store']);
+
+
+Route::get('games/edit/{id}', [GameController::class, 'edit']);
+Route::post('games/update/{id}', [GameController::class, 'update']);
+
+
+Route::post('games/destroy/{id}', [GameController::class, 'destroy']);
+
+Route::get('/games/show/{id}', [GameController::class, 'show'])->name('games.show');
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
