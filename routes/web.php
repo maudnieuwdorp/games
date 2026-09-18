ad52b8050f984aaea0dcbdf85b8af9fb8c7a5ae4
maud <maudnieuwdorp4@gmail.com>
2026-09-18 09:35:56 +0200

    Git Difference View

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GameController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('games', [GameController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-Route::get('games/create', [GameController::class, 'create']);
-Route::post('games/store', [GameController::class, 'store']);
-
-
-Route::get('games/edit/{id}', [GameController::class, 'edit']);
-Route::post('games/update/{id}', [GameController::class, 'update']);
-
-
-Route::post('games/destroy/{id}', [GameController::class, 'destroy']);
-
-Route::get('/games/show/{id}', [GameController::class, 'show'])->name('games.show');
-Route::get('/games', [GameController::class, 'index'])->name('games.index');
-
+require __DIR__.'/auth.php';
